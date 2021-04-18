264ab14c6cf679e050abc3aabb1f6874a6396809
Michael <mgo518@berkeley.edu>
2021-04-17 20:20:57 -0500

    nice looking background and stuff

--- pomodoro/change_background.js
+++ pomodoro/change_background.js
@@ -1,8 +1,5 @@
 //Sets background color to given thingy ma bobber
 function set_background_color(desired_color){
-    body = document.getElementById("body");
-    body.setAttribute("style", "background-color: " + desired_color)
-    body.setAttribute("style", "background-image: none")
     document.body.style.backgroundImage = 'none';
     document.body.style.backgroundColor = desired_color;
 }
@@ -44,6 +41,14 @@ let background_12_button = document.getElementById("background-12");
 background_1_button.addEventListener("click", function() {set_background_color("aquamarine");});
 background_2_button.addEventListener("click", function() {set_background_color("cadetblue");});
 background_3_button.addEventListener("click", function() {set_background_color("crimson");});
-background_4_button.addEventListener("click", function() {set_background_picture("zero_two_4.jpg");});
-background_5_button.addEventListener("click", function() {set_background_picture("nature.jpg");});
-background_6_button.addEventListener("click", function() {set_background_picture("modern.webp");});
+background_4_button.addEventListener("click", function() {set_background_color("darkseagreen");});
+background_5_button.addEventListener("click", function() {set_background_color("blanchedalmond");});
+background_6_button.addEventListener("click", function() {set_background_color("plum");});
+
+background_7_button.addEventListener("click", function() {set_background_picture("zero_two_4.jpg");});
+background_8_button.addEventListener("click", function() {set_background_picture("tanjiro.jpg");});
+background_9_button.addEventListener("click", function() {set_background_picture("your_name.jpg");});
+
+background_10_button.addEventListener("click", function() {set_background_picture("nature.jpg");});
+background_11_button.addEventListener("click", function() {set_background_picture("modern.webp");});
+background_12_button.addEventListener("click", function() {set_background_picture("abstract.jpg");});
